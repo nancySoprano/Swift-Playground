3a76c7fff37ce8e19dbd9fde774e4e7df6854366
nancySoprano <kasiandraa@gmail.com>
2019-01-09 19:35:46 +0100

    power function instead of multiplying

---
+++ BMI Calculator.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+func BMI (height : Float, weight : Float) -> String {
+    let BMI = weight / pow(height,2)
+    
+    if BMI > 25 {
+        return "You are overweight. Your BMI is \(BMI)"
+    }
+    else if BMI >= 18.5 && BMI <= 25 {
+        return "Your weight is normal. Your BMI is \(BMI)"
+    }
+    else {
+        return "You are underweight. Your BMI is \(BMI)"
+    }
+}
+
+print (BMI (height: 1.7, weight: 70))
+
