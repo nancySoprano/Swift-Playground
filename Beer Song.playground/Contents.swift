cd24e3fb41d2aa3d64c0cfd711f7220ef182f39c
nancySoprano <kasiandraa@gmail.com>
2019-01-10 09:23:21 +0100

    Beer Song

---
+++ Beer Song.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+
+var beers = [Int]()        //create an empty array of integers
+
+func song () -> String {
+        //print("\number bottles of beer on the wall, \number bottles of beer. Take one down and pass it around, \number-1 bottles of beer on the wall.")
+    }
+    
+}
