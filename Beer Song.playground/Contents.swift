bced857040b63358da46cab1c9af9000e045feea
nancySoprano <kasiandraa@gmail.com>
2019-01-12 22:09:18 +0100

    the beer song

--- Beer Song.playground/Contents.swift
+++ Beer Song.playground/Contents.swift
@@ -1,10 +1,18 @@
 import UIKit
 
 
-var beers = [Int]()        //create an empty array of integers
-
-func song () -> String {
-        //print("\number bottles of beer on the wall, \number bottles of beer. Take one down and pass it around, \number-1 bottles of beer on the wall.")
+func song() -> String { //creating a new function that will return a String
+    var lyrics: String = "" //creating a new variable which is a String. for now it's empty. ~I will occupy it with the actual lyrics
+    
+    for number in 1...99 { //a for loop where it will loop through 1 to 99
+        let newLine: String = "\n\(100 - number) bottles of beer on the wall, \(100-number) bottles of beer. \nTake one down and pass it around, \(99-number) bottles of beer on the wall.\n"
+        lyrics += newLine
     }
     
+    lyrics += "\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+    
+    return(lyrics)
 }
+
+print(song())
+
