2c2fa5879b103cbc853cba130fc2406194852c66
nancySoprano <kasiandraa@gmail.com>
2019-01-12 23:06:09 +0100

    used the reversed() function and set up the amount of beer bottles as a parmeter instead of it being hard coded

--- Beer Song.playground/Contents.swift
+++ Beer Song.playground/Contents.swift
@@ -1,18 +1,18 @@
 import UIKit
 
 
-func song() -> String { //creating a new function that will return a String
+func song(amountOfBottles: Int) -> String { //creating a new function that will return a String. the functions takes a parameter "amountOfBottle" (integer)
     var lyrics: String = "" //creating a new variable which is a String. for now it's empty. ~I will occupy it with the actual lyrics
     
-    for number in 1...98 { //a for loop where it will loop through 1 to 99
-        let newLine: String = "\n\(100 - number) bottles of beer on the wall, \(100-number) bottles of beer. \nTake one down and pass it around, \(99-number) bottles of beer on the wall.\n"
+    for number in (2...amountOfBottles).reversed() { //a for loop through 2 to the amount requested (not 1 because the lyrics would be gramatically incorect
+        let newLine: String = "\n\(number) bottles of beer on the wall, \(number) bottles of beer. \nTake one down and pass it around, \(number - 1) bottles of beer on the wall.\n"
         lyrics += newLine
     }
     
-    lyrics += "\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+    lyrics += "\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall." //add this line after loop ended
     
     return(lyrics)
 }
 
-print(song())
+print(song(amountOfBottles: 99)) //giving the parameter "amountOfBottles" a value
 
