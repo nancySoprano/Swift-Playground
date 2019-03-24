fd5c78cab794d7c3c0279bad5b4db4d41ccd949f
nancySoprano <kasiandraa@gmail.com>
2019-03-24 15:44:49 +0100

    String Interpolation. + Bronze Challenge

--- Big Nerd Ranch/MyPlayground1.playground/Contents.swift
+++ Big Nerd Ranch/MyPlayground1.playground/Contents.swift
@@ -5,7 +5,8 @@ var population: Int = 34068
 //population = 34067
 //print(number)
 let townName: String = "Nancy Ville"
-let townDescriton = "\(townName) has a population of \(population) and \(number) restaurants."
+var unemploymentRate: Float = 3.9
+let townDescriton = "\(townName) has a population of \(population) and \(number) restaurants. The unemployment rate is equal to \(unemploymentRate)%." //string interpolation
 print(townDescriton)
-
+//test comment test commit
 //„The \() syntax represents a placeholder in the String literal that accesses an instance’s value and places it within the new String. For example, \(townName) accesses the constant townName’s value and places it within the new String instance.”
