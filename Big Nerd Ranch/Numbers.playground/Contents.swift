cd0408b11fbaef44e272e6b8784f424b36c1ad2a
nancySoprano <kasiandraa@gmail.com>
2019-03-24 17:23:01 +0100

    4. Numbers. learned about signed / unsigned integers.

--- Big Nerd Ranch/Numbers.playground/Contents.swift
+++ Big Nerd Ranch/Numbers.playground/Contents.swift
@@ -1,5 +1,15 @@
 import Cocoa
 
-print("The maximum Int value is \(Int.max).")
-print("The minimum Int value is \(Int.min).")
+print("The maximum Int value is \(Int32.max).")
+print("The minimum Int value is \(Int32.min).")
+
+//signed : both positive or negative
+//unsigned: positive. minimum value 0.
+
+print("The maximum value for unsigned Int is \(UInt.max).")
+
+//„Swift style is to prefer Int for all integer uses”
+
+
+
 
