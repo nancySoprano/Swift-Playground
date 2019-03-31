a66a0d33903334cf4d9fc39dd72e8247469dc2af
nancySoprano <kasiandraa@gmail.com>
2019-03-31 21:42:07 +0200

    value binding

--- Big Nerd Ranch/Switch.playground/Contents.swift
+++ Big Nerd Ranch/Switch.playground/Contents.swift
@@ -1,18 +1,21 @@
 import Cocoa
 
-var statusCode: Int = 403
+var statusCode: Int = 1
 var errorString: String = "The request failed with the error: "
 switch statusCode {
 case 100, 101:
-    errorString += "Informational, 1xx."
+    errorString += "Informational, \(statusCode)"
 case 204:
-    errorString += "Successful but no content, 204."
+    errorString += "Successful but no content, \(statusCode)."
 case 300...307: //range matching
-    errorString += "Redirection, 3xx."
+    errorString += "Redirection, \(statusCode)."
 case 400...417:
-    errorString += "Client error, 4xx."
-default:
-    errorString = "Unknown. Please review the request and try again."
+    errorString += "Client error, \(statusCode)."
+//default:
+//    errorString = "Unknown. Please review the request and try again."
+
+case let unknownCode: //takes ion the value of any status code that does not match the earlier cases
+    errorString = "\(unknownCode) is not a known error code."
 }
 
 print(errorString)
